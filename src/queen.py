5b4a00caef0c3c86b7748b6e024fe63cd4b5b837
esadakcam <m.esadakcam@hotmail.com>
2022-02-11 21:45:55 +0300

    queen is implemented

--- src/queen.py
+++ src/queen.py
@@ -1,6 +1,13 @@
-from piece import Piece
+from bishop import Bishop
+from rook import Rook
 
 
-class Queen(Piece):
+class Queen(Bishop, Rook):
     def __init__(self, img, x, y, color, name):
-        super().__init__(img, x, y, color, name)
+        Bishop.__init__(self, img, x, y, color, name)
+        Rook.__init__(self, img, x, y, color, name)
+
+    def available_moves(self, board, previous_turn_piece):
+        bishop_moves = Bishop.available_moves(self, board, previous_turn_piece)
+        rook_moves = Rook.available_moves(self, board, previous_turn_piece)
+        return bishop_moves + rook_moves
