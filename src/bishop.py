74b990dcc5f301883ac20ff56f47f50c26eab9c7
esadakcam <m.esadakcam@hotmail.com>
2022-02-11 20:17:37 +0300

    bishop is stable

--- src/bishop.py
+++ src/bishop.py
@@ -6,16 +6,41 @@ class Bishop(Piece):
         super().__init__(img, x, y, color, name)
         self.square = square
 
-    def update(self, board, x, y, previous_turn_piece):
-        pass
-
     def available_moves(self, board, previous_turn_piece):
         moves = []
-        for i in range(8):
-            if self.board_x + i > 8 or self.board_y + i > 8:
+        for i in range(1, 8):
+            if self.board_x + i > 7 or self.board_y + i > 7:
                 break   # out of bounds
             if board.get_piece_by_index(self.board_x + i, self.board_y + i) is not None:
                 if board.get_piece_by_index(self.board_x + i, self.board_y + i).color != self.color:
                     moves.append((self.board_x + i, self.board_y + i))
                 break
             moves.append((self.board_x + i, self.board_y + i))
+
+        for i in range(1, 8):
+            if self.board_x + i > 7 or self.board_y - i < 0:
+                break   # out of bounds
+            if board.get_piece_by_index(self.board_x + i, self.board_y - i) is not None:
+                if board.get_piece_by_index(self.board_x + i, self.board_y - i).color != self.color:
+                    moves.append((self.board_x + i, self.board_y - i))
+                break
+            moves.append((self.board_x + i, self.board_y - i))
+
+        for i in range(1, 8):
+            if self.board_x - i < 0 or self.board_y - i < 0:
+                break   # out of bounds
+            if board.get_piece_by_index(self.board_x - i, self.board_y - i) is not None:
+                if board.get_piece_by_index(self.board_x - i, self.board_y - i).color != self.color:
+                    moves.append((self.board_x - i, self.board_y - i))
+                break
+            moves.append((self.board_x - i, self.board_y - i))
+
+        for i in range(1, 8):
+            if self.board_x - i < 0 or self.board_y + i < 0:
+                break   # out of bounds
+            if board.get_piece_by_index(self.board_x - i, self.board_y + i) is not None:
+                if board.get_piece_by_index(self.board_x - i, self.board_y + i).color != self.color:
+                    moves.append((self.board_x - i, self.board_y + i))
+                break
+            moves.append((self.board_x - i, self.board_y + i))
+        return moves
